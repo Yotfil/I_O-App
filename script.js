43e93617bc15d5aceb6a4b43edaa3066fdd596b1
Jefry Edgardo Guevara Vasquez <jeguevara@ifxcorp.com>
2021-01-24 16:18:33 -0500

    character creation

--- script.js
+++ script.js
@@ -210,7 +210,7 @@ function askConfirm(arma){
     if(character.race !== 'Mago'){
         arma = 'Arma'
     }else{
-        arma = 'Poder'
+        arma = 'Poders mágicos'
     }
 
     const confirm = askConfirm(arma)
